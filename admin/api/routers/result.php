eb36be15fef47ed9fe63d9c6a5fae50f7b8d7298
lolipopmari <mit.maria2019@yandex.ru>
2022-12-22 15:23:22 +0300

    написан первый вариант расчета по нелдеру миду

--- admin/api/routers/result.php
+++ admin/api/routers/result.php
@@ -13,7 +13,7 @@ function getResult($fData) {
     // $funcFormat = str_replace("^", "**", $fData['func']);
     // echo $funcFormat;
 
-    // расчет симплекса
+    // шаг 1  -  расчет симплекса
     $n = $fData['countx'];
     $l = $fData['lambda'] ?: 1;
     $delta1 = ((sqrt($n + 1) + $n - 1) / ($n * sqrt(2))) * $l;
@@ -42,22 +42,17 @@ function getResult($fData) {
     $flag = true;
     $iteration = 0;
     // $maxIteration = 1000;
-    $maxIteration = 1;
+    $maxIteration = 3;
 
     
 
     while($flag && $iteration < $maxIteration) {
+        $iteration++;
+
+        // шаг 2   -  просчет значений всех функций
         $fResult = [];
 
-        $xl = $simplex[0];        
-        $xg = [];
-        $xh = $simplex[0];
-
-        $resXl = 0;
-        $resXh = 0;
-
         foreach($simplex as $key => $t) {
-            var_dump($t);
             try {
                 $compiler = new FormulaInterpreter\Compiler();
                 $executable = $compiler->compile($fData['func']);
@@ -66,30 +61,118 @@ function getResult($fData) {
                 echo $e->getMessage(), "\n";
             }
             $fResult[] = $result;
-            // try {
-            //     $parser = new FormulaParser($fData['func'], 3);
-            //     $parser->setVariables($t);
-            //     $result = $parser->getResult(); // [0 => 'done', 1 => 16.38]
-            // } catch (\Exception $e) {
-            //     echo $e->getMessage(), "\n";
-            // }
-            // $fResult[] = $result;
-
-            // $funcFormatLocal = $funcFormat;
-
-            // // подготовка формулы для вычисления
-            // for($i = $n; $i > 0; $i--) {
-            //     $funcFormatLocal = str_replace("x$i", $t[$i - 1], $funcFormatLocal);
-            // }
-
-            // print_r($funcFormatLocal);
-
-            // $fResult[] = (int) $funcFormatLocal;
         }
 
-        $iteration++;
+        // переопредление точек
+        $indexMin = array_keys($fResult, min($fResult))[0];
+        $indexMax = array_keys($fResult, max($fResult))[0];
+
+        $xl = $simplex[$indexMin];
+        $xh = $simplex[$indexMax];
+        $xg = $simplex;
+
+        unset($xg[$indexMin]);
+        unset($xg[$indexMax]);
+
+        // шаг 3  - расчет центра тяжести - без xh
+        $xc = [];
+
+        foreach($xl as $key => $coord) {
+            $xc[$key] = $coord / $n;
+        }
+
+        foreach($xg as $xi) {
+            foreach($xi as $key => $coord) {
+                $xc[$key] += $coord / $n;
+            }
+        }
+        // расчет значения fc
+        $fc = $executable->run($xc);
+
+        // шаг 4 - поиск пробной точки xr
+        $xr = [];
+
+        foreach($xc as $key => $coord) {
+            $xr[$key] = (1 + $fData['alpha']) * $coord - $fData['alpha'] * $xh[$key];
+        }
+        $fr = $executable->run($xr);
+
+        // шаг 5  -  сравнение fr и fl
+        $tmpfResult = $fResult;
+        unset($tmpFResult[$indexMin]); 
+        unset($tmpFResult[$indexMax]); 
+
+        if($fr < $fResult[$indexMin] && $fr != $fResult[$indexMin]) {
+            // шаг 5.1  - расчет xe
+            $xe = [];
+
+            foreach($xr as $key => $coord) {
+                $xe[$key] = ($fData['gamma'] * $coord) - ((1 - $fData['gamma']) * $xc[$key]);
+            }
+            $fe = $executable->run($xe);
+
+            if($fe < $fr) {
+                $simplex[$indexMax] = $xe;
+            } else {
+                $simplex[$indexMax] = $xr;
+            }
+
+            print_r($xe);
+
+        } elseif($fr >= $fResult[$indexMin] || $fr <= max($tmpfResult)) {
+            // шаг 5.2  - замена xh на xr
+            $simplex[$indexMax] = $xr;
+        } else {
+            // шаг 6 - сжатие многогранника
+            $xs = [];
+
+            if($fResult[$indexMax] <= $fr) {
+                // вычисляем xs от xc и xh
+                foreach($xh as $key => $coord) {
+                    $xs[$key] = ($fData['betta'] * $coord) - ((1 - $fData['betta']) * $xc[$key]);
+                }
+            } else {
+                // вычисляем xs от xc и xr
+                foreach($xr as $key => $coord) {
+                    $xs[$key] = ($fData['betta'] * $coord) - ((1 - $fData['betta']) * $xc[$key]);
+                }
+            }
+            
+            $fs = $executable->run($xs);
+
+            // шаг 6 - сравнение fs и fr, fh
+            if($fs < min($fr, $fResult[$indexMax])) {
+                $simplex[$indexMax] = $xs;
+            } elseif($fs >= $fResult[$indexMax]) {
+                // шаг 8 - редукция симплекса
+                foreach($simplex as $i => $xi) {
+                    if($i == $indexMin) continue;
+
+                    foreach($xi as $key => $coord) {
+                        $xi[$key] = $xl[$key] + 0.5 * ($xi[$key] - $xl[$key]);
+                    }
+
+                    $simplex[$i] = $xi;
+                }
+            }
+        }
+
+        // проверка критерия останова
+        $fsh = (1 / ($n+1)) * array_sum($xc);
+        $sigma = 0;
+        foreach($simplex as $i => $xi) {
+            $sigma += (1 / ($n+1)) * pow((array_sum($xi) - $fsh), 2);
+        }
+        $sigma = sqrt($sigma);
+
+        if($sigma <= $fData['epx']){
+            $flag = false;
+        }
+
+        echo "iteration " . ($iteration - 1);
+        // print_r($simplex);
     }
     
-    print_r($fResult);
+    
     
 }
